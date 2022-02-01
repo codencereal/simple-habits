8de4bbd9849f129f95405fde16435a8f021df36d
Jaidon Nelson <jaidonkdnelson@gmail.com>
2022-02-01 09:54:13 -0400

    More linting

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -17,7 +17,7 @@ class HomePage extends StatelessWidget {
                   builder: (context) => const SettingsPage()));
             },
             icon: const Icon(Icons.settings),
-          )
+          ),
         ],
       ),
       drawer: Drawer(
@@ -26,19 +26,20 @@ class HomePage extends StatelessWidget {
               0xFFA2A2A2), //TODO: Add a better colour to theme and use that colour instead
           child: ListView(children: const [
             DrawerHeader(
-                child: Center(
-              child: Text(
-                'H A B I T S',
-                style: TextStyle(fontSize: 35),
+              child: Center(
+                child: Text(
+                  'H A B I T S',
+                  style: TextStyle(fontSize: 35),
+                ),
               ),
-            )),
+            ),
             ListTile(
               leading: Icon(Icons.home),
               title: Text(
                 'Page 1',
                 style: TextStyle(fontSize: 20),
               ),
-            )
+            ),
           ]),
         ),
       ),
